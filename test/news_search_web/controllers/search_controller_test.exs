e8752b44d6cbfaae2db121fc53211e910bafa535
Ravern Koh <ravern.koh.dev@gmail.com>
2017-10-03 21:57:54 +0800

    Wrote tests

--- test/news_search_web/controllers/search_controller_test.exs
+++ test/news_search_web/controllers/search_controller_test.exs
@@ -1,3 +1,17 @@
 defmodule NewsSearchWeb.SearchControllerTest do
   use NewsSearchWeb.ConnCase
+
+  test "GET / shows form", %{conn: conn} do
+    conn = get conn, "/"
+    response = html_response(conn, 200)
+    assert response =~ "SG News Search"
+    assert response =~ "Search"
+  end
+
+  test "GET /search shows form", %{conn: conn} do
+    conn = get conn, "/"
+    response = html_response(conn, 200)
+    assert response =~ "SG News Search"
+    assert response =~ "Search"
+  end
 end
